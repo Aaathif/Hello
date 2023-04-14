a5af2c0a2cfe6c924b95fed2390b6531f9e6f0a5
Aathif <aaathif115@gmail.com>
2023-04-14 07:04:17 +0530

    frist admin commit

---
+++ src/pages/newProduct/NewProduct.jsx
@@ -0,0 +1,142 @@
+import "./newProduct.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import { useState } from "react";
+import axios from "axios";
+import useFetch from "../../hooks/useFetch";
+
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
+  const [info, setInfo] = useState({});
+  const [prices, setPrices] = useState([]);
+  const [sizes, setSizes] = useState([]);
+
+  const handleSizeChange = (event) => {
+    const { value } = event.target;
+    const newSizes = value.split(',');
+    setSizes(newSizes);
+  };
+
+  const { data, loading, error } = useFetch("/products");
+
+  const handleChange = (e) => {
+    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleSelect = (e) => {
+    const value = Array.from(
+      e.target.selectedOptions,
+      (option) => option.value
+    );
+    setPrices(value);
+  };
+
+  const handleClick = async (e) => {
+    alert("Hello submitted")
+    e.preventDefault();
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "upload");
+    try {
+      const uploadRes = await axios.post(
+        "https://api.cloudinary.com/v1_1/dbzy3p0c7/image/upload",
+        data
+      );
+
+      const { url } = uploadRes.data;
+
+      const newProduct = {
+        ...info,
+        sizes,
+        photos: url, 
+      };
+
+      await axios.post("http://localhost:8000/api/product", newProduct);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(info);
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
+              </div>
+
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input
+                    onChange={handleChange}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    id={input.id}
+                  />
+                </div>
+              ))}
+{/* -------------------------------------------------------------------------------------- */}
+              <div className="formInput">
+                  <label htmlFor="size-input">Enter sizes:</label>
+                      <input
+                        id="size"
+                        type="text"
+                        value={sizes.join(',')}
+                        onChange={handleSizeChange}
+                    />
+              </div>
+
+{/* -------------------------------------------------------------------------------------- */}
+
+              <div className="selectRooms">
+                <label>Prices</label>
+                <select id="prices" multiple onChange={handleSelect}>
+                  {loading
+                    ? "loading"
+                    : data &&
+                      data.map((room) => (
+                        <option key={room._id} value={room._id}>
+                          {room.prices}
+                        </option>
+                      ))}
+                </select>
+              </div>
+
+              <button onClick={handleClick}>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default New;
